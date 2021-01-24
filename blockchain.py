56a9a3f70b1e64f1d0b9ca4b4846807a7399cbe2
Ezequiel Melillan <ezequielmelillan@gmail.com>
2021-01-24 00:55:36 -0300

    initial commit

---
+++ blockchain.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: "utf-8"
+
+from hashlib import sha256
+
+def updatehash(*args):
+    hashing_text = ""
+    h = sha256()
+    for arg in args:
+        hashing_text += str(arg)
+    
+    h.update(hashing_text.encode('utf-8'))
+    return h.hexdigest()
+
+
+class Block():
+    data = None
+    hash = None
+    nonce = 0
+    previous_hash = "0" * 64
+
+    def __init__(self, data, number=0):
+        self.data = data
+        self.number = number
+
+    def hash(self):
+        return updatehash(self.previous_hash, self.number, self.data, self.nonce)
+
+    def __str__(self):
+        return str(f"Block#: {self.number} \n"
+                   f"Hash: {self.hash()} \n"
+                   f"Previous: {self.previous_hash} \n"
+                   f"Data: {self.data} \n"
+                   f"Nonce: {self.nonce}")
+
+
+class Blockchain():
+    difficulty = 4
+
+    def __init__(self, chain = []):
+        self.chain = chain
+
+    def add(self, block):
+        self.chain.append({
+            "hash" : block.hash(),
+            "previous" : block.previous_hash,
+
+
+        })
+
+
+def main():
+    block = Block("Hello World",1)
+    print(block)
+
+
+if __name__ == "__main__":
+    main()
